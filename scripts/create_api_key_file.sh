d9e19a6908f85e350b292ae401fb0228da693b48
stotic-dev <taichis844@gmail.com>
2025-04-24 09:27:24 +0900

    APIキーパスを環境変数であらかじめ設定するように修正

--- scripts/create_api_key_file.sh
+++ scripts/create_api_key_file.sh
@@ -2,18 +2,16 @@
 
 # 引数チェック
 if [ "$#" -ne 3 ]; then
-  echo "Usage: $0 <base_directory> <api_key_id> <api_key_base64>"
+  echo "Usage: $0 <path> <base_dir> <api_key_base64>"
   exit 1
 fi
 
 # 引数の割り当て
-BASE_DIR="$1"
-API_KEY_ID="$2"
+API_KEY_FILE_PATH="$1"
+BASE_DIR="$2"
 API_KEY_BASE_64="$3"
 
-# ASCのAPIキーファイル生成処理を行って、環境変数に設定する
+# ASCのAPIキーファイル生成処理を行う
 mkdir -p $BASE_DIR
-API_KEY_PATH="$BASE_DIR/AuthKey_$API_KEY_ID.p8"
-echo "$API_KEY_BASE_64" | base64 --decode > "$API_KEY_PATH"
-chmod 600 "$API_KEY_PATH"
-export API_KEY_PATH
+echo "$API_KEY_BASE_64" | base64 --decode > "$API_KEY_FILE_PATH"
+chmod 600 "$API_KEY_FILE_PATH"
