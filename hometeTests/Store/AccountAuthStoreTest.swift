16827fca4a7e41d19aa7c396efa1052eb4507b63
stotic-dev <taichis844@gmail.com>
2025-08-11 20:20:16 +0900

    update: ログアウトの単体テストを追加

--- hometeTests/Store/AccountAuthStoreTest.swift
+++ hometeTests/Store/AccountAuthStoreTest.swift
@@ -5,6 +5,7 @@
 //  Created by 佐藤汰一 on 2025/08/09.
 //
 
+import os
 import Testing
 @testable import homete
 
@@ -47,5 +48,38 @@ struct AccountAuthStoreTest {
             
             try await store.login(.init(tokenId: inputTokenId, nonce: inputNonce))
         }
+    }
+    
+    @Test("ログアウト時はローカルのログイン状態をログアウトにしてログアウト処理を行い、イベントログを送信する")
+    func test_logout() throws {
+        
+        let isCallSignOut = OSAllocatedUnfairLock(initialState: false)
+        let isCallAnalyticsLog = OSAllocatedUnfairLock(initialState: false)
+        
+        let store = AccountAuthStore(appDependencies: .init(
+            accountAuthClient: .init(signIn: { _, _ in
+                
+                Issue.record()
+                return .init(id: "", displayName: "")
+            },
+                                     signOut: { isCallSignOut.withLock { $0 = true } },
+                                 makeListener: { .defaultValue() }),
+            analyticsClient: .init(setId: { _ in
+                
+                Issue.record()
+            }, log: { event in
+                
+                isCallAnalyticsLog.withLock { $0 = true }
+                #expect(event == .logout())
+            })
+        ))
+        store.auth = .init(id: "test", displayName: "test")
+        
+        store.logOut()
+        
+        #expect(isCallSignOut.withLock { $0 })
+        #expect(isCallAnalyticsLog.withLock { $0 })
+        #expect(store.auth == nil)
+        #expect(!store.isLogin)
     }
 }
