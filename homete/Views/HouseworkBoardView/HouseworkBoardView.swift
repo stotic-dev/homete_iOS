aac4bfc344b91812ab33a5f2c23167ae620ee0ba
stotic-dev <taichis844@gmail.com>
2025-09-06 14:02:57 +0900

    update: セグメントボタンの変更に合わせて家事リストを出し分ける

--- homete/Views/HouseworkBoardView/HouseworkBoardView.swift
+++ homete/Views/HouseworkBoardView/HouseworkBoardView.swift
@@ -10,13 +10,13 @@ import SwiftUI
 struct HouseworkBoardView: View {
     
     @State var selectedHouseworkState: HouseworkState = .incomplete
-    @State var houseworkItems: [HouseworkItem] = []
+    @State var houseworkBoardList: HouseworkBoardList = .init(items: [])
     
     var body: some View {
         VStack(spacing: DesignSystem.Space.space16) {
             HouseworkBoardSegmentedControl(selectedHouseworkState: $selectedHouseworkState)
             List {
-                ForEach(houseworkItems) { item in
+                ForEach(houseworkBoardList.items(matching: selectedHouseworkState)) { item in
                     Button {
                         // TODO: 承認依頼を行う
                     } label: {
@@ -30,6 +30,7 @@ struct HouseworkBoardView: View {
                 }
                 .listRowSpacing(.zero)
                 .listRowSeparator(.hidden)
+                // TODO: 左スワイプで家事を削除する
             }
             .listStyle(.plain)
             Spacer()
@@ -40,11 +41,13 @@ struct HouseworkBoardView: View {
 
 #Preview {
     HouseworkBoardView(
-        houseworkItems: [
-            .init(id: "1", title: "洗濯", state: .incomplete),
-            .init(id: "2", title: "ゴミ捨て", state: .pendingApproval),
-            .init(id: "3", title: "風呂掃除", state: .completed)
-        ]
+        houseworkBoardList: .init(
+            items: [
+                .init(id: "1", title: "洗濯", state: .incomplete),
+                .init(id: "2", title: "ゴミ捨て", state: .pendingApproval),
+                .init(id: "3", title: "風呂掃除", state: .completed)
+            ]
+        )
     )
     .apply(theme: .init())
 }
