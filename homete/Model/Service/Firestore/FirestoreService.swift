65d634847733f64dfae0409e43bed4cac64a6dd0
stotic-dev <taichis844@gmail.com>
2025-10-04 08:38:08 +0900

    update: FirestoreServiceのファイルにあった異なる型の定義を別ファイルに切り出し

--- homete/Model/Service/Firestore/FirestoreService.swift
+++ homete/Model/Service/Firestore/FirestoreService.swift
@@ -5,26 +5,7 @@
 //  Created by 佐藤汰一 on 2025/08/09.
 //
 
-import Combine
 import FirebaseFirestore
-
-protocol FirestoreListenerStorerable<Element> {
-    associatedtype Element
-    var continuation: AsyncStream<Element>.Continuation { get }
-    var listener: any ListenerRegistration { get }
-    func remove()
-}
-
-struct FirestoreListener<Element>: FirestoreListenerStorerable {
-    let continuation: AsyncStream<Element>.Continuation
-    let listener: any ListenerRegistration
-    
-    func remove() {
-        
-        continuation.finish()
-        listener.remove()
-    }
-}
 
 final actor FirestoreService {
     
@@ -54,19 +35,25 @@ final actor FirestoreService {
         id: String,
         predicate: (Firestore) -> Query
     ) -> AsyncStream<[Output]> where Output: Decodable {
+        
         let (stream, continuation) = AsyncStream.makeStream(
             of: [Output].self,
             bufferingPolicy: .bufferingNewest(10)
         )
         let listener = predicate(firestore)
             .addSnapshotListener { snapshots, error in
+                
                 if let error {
+                    
+                    print("occurred error at fetchSnapshotListener(type: \(Output.self), error: \(error))")
                     return
                 }
+                
                 guard let snapshots else { return }
                 let convertedValues = snapshots.documents.compactMap { try? $0.data(as: Output.self) }
                 continuation.yield(convertedValues)
             }
+        
         listeners[id] = FirestoreListener(continuation: continuation, listener: listener)
         return stream
     }
@@ -76,38 +63,4 @@ final actor FirestoreService {
         let listener = listeners[id]
         listener?.remove()
     }
-}
-
-extension Firestore {
-    
-    /// アカウントの参照を取得する
-    func accountRef(id: String) -> DocumentReference {
-        
-        return self
-            .collection(CollectionPath.account.rawValue)
-            .document(id)
-    }
-    
-    /// 同居人の参照を取得する
-    func cohabitantRef(id: String) -> DocumentReference {
-        
-        return self
-            .collection(CollectionPath.cohabitant.rawValue)
-            .document(id)
-    }
-    
-    /// 家事コレクションの参照を取得する
-    func houseworkListRef(id: String) -> CollectionReference {
-        
-        return self.cohabitantRef(id: id)
-            .collection(CollectionPath.houseworks.rawValue)
-    }
-}
-
-enum CollectionPath: String {
-    
-    case account = "Account"
-    case cohabitant = "Cohabitant"
-    case houseworks = "Houseworks"
-    case dailyHouseworks = "DailyHouseworks"
 }
