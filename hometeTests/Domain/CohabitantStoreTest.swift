ee80db551c98942b50f85abbb70e403c203e1e02
stotic-dev <taichis844@gmail.com>
2026-01-12 09:29:17 +0900

    fix: テスト時のコマンドは不要なので削除

---
+++ hometeTests/Domain/CohabitantStoreTest.swift
@@ -0,0 +1,16 @@
+//
+//  CohabitantStoreTest.swift
+//  hometeTests
+//
+//  Created by Taichi Sato on 2026/01/12.
+//
+
+import Testing
+
+struct CohabitantStoreTest {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
