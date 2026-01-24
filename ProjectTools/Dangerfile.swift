319d7336b8fbeb5c269bcef05db59f1bb52b0c5c
stotic-dev <taichis844@gmail.com>
2026-01-24 15:51:38 +0900

    update: 追加のsnapshotも表示するようにする

--- ProjectTools/Dangerfile.swift
+++ ProjectTools/Dangerfile.swift
@@ -14,24 +14,37 @@ if let github = danger.github {
     }
     
     // VRTのsnapshotから画像の差分を表示する
+    let repoSlug = danger.github.pullRequest.base.repo.fullName // 例: "owner/repo"
+    let headCommitSha = danger.github.pullRequest.head.sha // 変更後
+    let baseCommitSha = danger.github.pullRequest.base.sha // 変更前
     let vrtSnapshotDir = "hometeSnapshotTests/__Snapshots__/PreviewTests.generated"
-    let changedSnapshotFiles = danger.git.modifiedFiles.filter{ $0.contains(vrtSnapshotDir) && $0.lowercased().hasSuffix(".png")
-    }
 
-    for imagePath in changedSnapshotFiles {
-        let repoSlug = danger.github.pullRequest.base.repo.fullName // 例: "owner/repo"
-        let headCommitSha = danger.github.pullRequest.head.sha // 変更後
-        let baseCommitSha = danger.github.pullRequest.base.sha // 変更前
-        
+    markdown("## snapshotの変更")
+    for imagePath in danger.git.modifiedFiles.filter({ $0.contains(vrtSnapshotDir) && $0.lowercased().hasSuffix(".png")
+    }) {
         // GitHubのraw URLを構築
         let beforeImageUrl = "https://raw.githubusercontent.com/\(repoSlug)/\(baseCommitSha)/\(imagePath)"
         let afterImageUrl = "https://raw.githubusercontent.com/\(repoSlug)/\(headCommitSha)/\(imagePath)"
         
         markdown("""
-        ### snapshotの変更: `\(imagePath)`
+        ### 更新ファイル: `\(imagePath)`
         | before | after |
         | ------ | ----- |
         | ![image](\(beforeImageUrl)) | ![image](\(afterImageUrl)) |
+        """)
+    }
+    
+    markdown("## snapshotの追加")
+    for imagePath in danger.git.createdFiles.filter({ $0.contains(vrtSnapshotDir) && $0.lowercased().hasSuffix(".png")
+    }) {
+        // GitHubのraw URLを構築
+        let addedImageUrl = "https://raw.githubusercontent.com/\(repoSlug)/\(headCommitSha)/\(imagePath)"
+        
+        markdown("""
+        ### 追加ファイル: `\(imagePath)`
+        | current |
+        | ------ |
+        | ![image](\(addedImageUrl)) |
         """)
     }
 }
