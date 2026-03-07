f52495f30e8c23fc9d1fa9a20211695b267e4a72
stotic-dev <taichis844@gmail.com>
2026-03-07 10:54:01 +0900

    refactor: SwiftLintをLocalPackageにも適用できるようにPlugin化

--- ProjectTools/Package.swift
+++ ProjectTools/Package.swift
@@ -8,30 +8,38 @@ let package = Package(
     platforms: [.macOS(.v10_15)],
     products: [
         .library(
-            name: "ProjectTools",
-            targets: ["ProjectTools"]
-        ),
-        .library(
             name: "DangerDeps",
             type: .dynamic,
             targets: ["DangerDeps"]
-        )
+        ),
+        .plugin(
+            name: "SwiftLintPlugin",
+            targets: ["SwiftLintPlugin"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/SwiftLint.git", exact: "0.59.1"),
         .package(url: "https://github.com/danger/swift.git", exact: "3.22.0"),
         .package(url: "https://github.com/f-meloni/danger-swift-coverage", from: "1.2.1"),
     ],
     targets: [
-        .target(
-            name: "ProjectTools"
-        ),
         .target(
             name: "DangerDeps",
             dependencies: [
                 .product(name: "Danger", package: "swift"),
                 .product(name: "DangerSwiftCoverage", package: "danger-swift-coverage"),
             ]
-        )
+        ),
+        .plugin(
+            name: "SwiftLintPlugin",
+            capability: .buildTool(),
+            dependencies: [
+                .target(name: "SwiftLintBinary"),
+            ]
+        ),
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.59.1/SwiftLintBinary.artifactbundle.zip",
+            checksum: "b9f915a58a818afcc66846740d272d5e73f37baf874e7809ff6f246ea98ad8a2"
+        ),
     ]
 )
