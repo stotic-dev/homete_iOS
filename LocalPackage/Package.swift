6960951cb64088c8e81706a24c6a977d989c577c
stotic-dev <taichis844@gmail.com>
2026-03-07 16:48:23 +0900

    refactor: ios/macOS環境以外ではHometeDomainのみをビルド対象にする

--- LocalPackage/Package.swift
+++ LocalPackage/Package.swift
@@ -3,6 +3,45 @@
 
 import PackageDescription
 
+// HometeUI・HometeResources はSwiftUI依存のためiOS・macOS以外では除外する
+#if os(iOS) || os(macOS)
+let iosOnlyDependencies: [Package.Dependency] = [
+    .package(url: "https://github.com/SwiftGen/SwiftGenPlugin", from: "6.6.2")
+]
+let iosOnlyProducts: [Product] = [
+    .library(
+        name: "HometeUI",
+        targets: ["HometeUI"]
+    ),
+    .library(
+        name: "HometeResources",
+        targets: ["HometeResources"]
+    ),
+]
+let iosOnlyTargets: [Target] = [
+    .target(
+        name: "HometeUI",
+        dependencies: [
+            "HometeDomain",
+            "HometeResources",
+        ],
+        plugins: [
+            .plugin(name: "SwiftLintPlugin", package: "ProjectTools"),
+        ]
+    ),
+    .target(
+        name: "HometeResources",
+        plugins: [
+            .plugin(name: "SwiftGenPlugin", package: "SwiftGenPlugin")
+        ]
+    ),
+]
+#else
+let iosOnlyDependencies: [Package.Dependency] = []
+let iosOnlyProducts: [Product] = []
+let iosOnlyTargets: [Target] = []
+#endif
+
 let package = Package(
     name: "LocalPackage",
     platforms: [.iOS(.v17), .macOS(.v15)],
@@ -11,40 +50,15 @@ let package = Package(
             name: "HometeDomain",
             targets: ["HometeDomain"]
         ),
-        .library(
-            name: "HometeUI",
-            targets: ["HometeUI"]
-        ),
-        .library(
-            name: "HometeResources",
-            targets: ["HometeResources"]
-        ),
-    ],
+    ] + iosOnlyProducts,
     dependencies: [
         .package(path: "../ProjectTools"),
-        .package(url: "https://github.com/SwiftGen/SwiftGenPlugin", from: "6.6.2")
-    ],
+    ] + iosOnlyDependencies,
     targets: [
         .target(
             name: "HometeDomain",
             plugins: [
                 .plugin(name: "SwiftLintPlugin", package: "ProjectTools"),
-            ]
-        ),
-        .target(
-            name: "HometeUI",
-            dependencies: [
-                "HometeDomain",
-                "HometeResources",
-            ],
-            plugins: [
-                .plugin(name: "SwiftLintPlugin", package: "ProjectTools"),
-            ]
-        ),
-        .target(
-            name: "HometeResources",
-            plugins: [
-                .plugin(name: "SwiftGenPlugin", package: "SwiftGenPlugin")
             ]
         ),
         .testTarget(
@@ -54,5 +68,5 @@ let package = Package(
                 .plugin(name: "SwiftLintPlugin", package: "ProjectTools"),
             ]
         ),
-    ]
+    ] + iosOnlyTargets
 )
