ee12018feff565b2118288d89b28cc5498eb5817
stotic-dev <taichis844@gmail.com>
2026-04-04 12:25:13 +0900

    feat: HouseworkDateStateをpublic化

--- LocalPackage/Sources/HometeDomain/Cohabitant/Housework/HouseworkDateState.swift
+++ LocalPackage/Sources/HometeDomain/Cohabitant/Housework/HouseworkDateState.swift
@@ -5,7 +5,7 @@
 //  Created by Taichi Sato on 2026/04/04.
 //
 
-enum HouseworkDateState: Equatable {
+public enum HouseworkDateState: Equatable {
     case selected
     case selectable
     case unselectable
