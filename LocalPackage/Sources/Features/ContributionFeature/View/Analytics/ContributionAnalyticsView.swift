802b491740efce936a80cedc8ce99a5477da2434
stotic-dev <taichis844@gmail.com>
2026-04-29 09:32:09 +0900

    ContributionAnalyticsViewの追加

---
+++ LocalPackage/Sources/Features/ContributionFeature/View/Analytics/ContributionAnalyticsView.swift
@@ -0,0 +1,18 @@
+//
+//  ContributionAnalyticsView.swift
+//  LocalPackage
+//
+//  Created by Taichi Sato on 2026/04/29.
+//
+
+import SwiftUI
+
+struct ContributionAnalyticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContributionAnalyticsView()
+}
