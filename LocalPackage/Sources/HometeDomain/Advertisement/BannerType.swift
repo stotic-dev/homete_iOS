a9e8667e1d258debb46a68de44fba206ad5d3caf
stotic-dev <taichis844@gmail.com>
2026-05-15 10:17:28 +0900

    feat: 広告のサイズとユニットIDをInfo.plist経由で受け取るように修正

---
+++ LocalPackage/Sources/HometeDomain/Advertisement/BannerType.swift
@@ -0,0 +1,11 @@
+//
+//  BannerType.swift
+//  LocalPackage
+//
+//  Created by Taichi Sato on 2026/05/15.
+//
+
+public enum BannerType {
+    /// ダッシュボードに表示するバナー
+    case dashboardTop
+}
---
+++ LocalPackage/Sources/HometeInfrastructure/Advertisement/AdType/BannerType+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  BannerType+Extension.swift
+//  LocalPackage
+//
+//  Created by Taichi Sato on 2026/05/15.
+//
+
+import Foundation
+import HometeDomain
+import GoogleMobileAds
+
+extension BannerType {
+    var unitId: String {
+        let adsUnitIdDic = Bundle.main.object(forInfoDictionaryKey: "AdUnitIdList") as? [String: String] ?? [:]
+        switch self {
+        case .dashboardTop:
+            return adsUnitIdDic["BANNER_DASHBOARD_TOP_AD_UNIT_ID"] ?? ""
+        }
+    }
+    
+    var size: AdSize {
+        switch self {
+        case .dashboardTop: AdSizeBanner
+        }
+    }
+}
