73c33fc1eddfa18beaa62af7bc2b5fe26c3fa39f
stotic-dev <taichis844@gmail.com>
2026-08-29 11:16:26 +0900

    fix: ライセンス一覧画面でナビゲーションバーのスクロールエッジエフェクトをsoftに固定

--- LocalPackage/Sources/Features/SettingFeature/LicenseList/LicenseListView.swift
+++ LocalPackage/Sources/Features/SettingFeature/LicenseList/LicenseListView.swift
@@ -19,6 +19,7 @@ struct LicenseListView: View {
         .listStyle(.plain)
         .navigationTitle("ライセンス")
         .inlineNavigationBarTitleDisplayMode()
+        .softTopScrollEdgeEffect()
     }
 
 }
